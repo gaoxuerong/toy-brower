e9b018f6001f39c61153aef825f1abdb982b949e
gaoxuerong <gaoxuerong123@gmail.com>
2020-05-27 15:07:38 +0800

    修正错误

--- server.js
+++ server.js
@@ -1,27 +1,23 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
-  console.log(req.headers)
-  res.setHeader('Content-Type', 'text/html');
-  res.setHeader('X-Foo', 'bar');
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end(
+    console.log(req.headers)
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('X-Foo', 'bar');
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    // writeHead 优先级高于setHeader，并且writeHead只能用一次;
+    res.end(
 `<html maaa=a >
 <head>
     <style>
 body div #myid{
     width:100px;
+    height: 100px;
     background-color: #ff5000;
-}
-body div img{
-    width:30px;
-    background-color: #ff1111;
 }
     </style>
 </head>
 <body>
-    <div>
-        <img id="myid"/>
-        <img />
+    <div id="myid">
     </div>
 </body>
 </html>`);
